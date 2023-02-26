176a5856c68c5d5fa7bfed0c99d221dba2fa1089
gabox2002 <gabox2002@gmail.com>
2023-02-26 16:02:45 -0300

    m

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
@@ -1,4 +1,58 @@
-function mostrar()
-{
-  alert("dos");
+/* Realizar el algoritmo que permita ingresar los datos de una compra productos de la construccion, hasta que el cliente quiera:
+Tipo validad("arena";"cal";"cemento")
+Cantidad de bolsas,
+Precio por bolsa (más de cero ),
+
+Si compro más de 10 bolsas en total tenes 15% de descuento sobre el total a pagar.
+Si compro más de 30 bolsas en total tenes 25% de descuento sobre el total a pagar.
+a) El importe total a pagar , bruto sin descuento y...
+b) el importe total a pagar con descuento(solo si corresponde)
+d) Informar el tipo con mas cantidad de bolsas.
+f) El tipo mas caro*/
+function mostrar() {
+    var tipo;
+    var respuesta;
+    var cantidadBolsas;
+    var precioBolsas;
+    var importeTotal;
+    var importeConDescuento;
+    var tipoMasCaro;
+    var descuentoMasDeDiez;
+    var descuentoMasDeTreinta;
+    
+    respuesta = true;
+
+    while (respuesta == true) {
+        tipo = prompt("Ingrese tipo de producto: arena, cemento o cal");
+
+        while (tipo != "arena" && tipo != "cemento" && tipo != "cal") {
+            tipo = prompt("Ingrese tipo de producto nuevamente: arena, cemento o cal");
+        }
+
+        cantidadBolsas = prompt("Ingrese cantidad de bolsas: ");
+        cantidadBolsas = parseInt(cantidadBolsas);
+        
+        precioBolsas = prompt("Ingrese el precio por bolsas");
+        precioBolsas = parseFloat(precioBolsas);
+
+        while(precioBolsas < 1){
+            precioBolsas = prompt("Ingrese el precio nuevamente");
+            precioBolsas = parseFloat(precioBolsas);
+        }
+       
+        respuesta = confirm("Desea continuar?");
+
+    }
+        if(cantidadBolsas > 10){
+            importeTotal = cantidadBolsas * precioBolsas;
+            descuentoMasDeDiez = importeTotal - (importeTotal * 0.15);
+        }
+        else{
+            if(cantidadBolsas > 30){
+                importeTotal = cantidadBolsas * precioBolsas;
+                descuentoMasDeTreinta = importeTotal - (importeTotal * 0.25);
+            }
+        }
+
+
 }
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
@@ -1,5 +1,137 @@
+/*
+Debemos realizar la carga de 5(cinco) productos de prevención de contagio,
+de cada una debo obtener los siguientes datos:
+el tipo (validar "barbijo" , "jabón" o "alcohol") ,
+el precio (validar entre 100 y 300),
+la cantidad de unidades (no puede ser 0 o negativo y no debe superar las 1000 unidades),
+la Marca y el fabricante.
+Se debe Informar al usuario lo siguiente:
+a) Del más barato de los alcohol, la cantidad de unidades y el fabricante
+b) Del tipo con mas unidades, el promedio por compra
+c) Cuántas unidades de jabones hay en total
+*/
 
 function mostrar()
 {
-	alert("uno");
+	var contador;
+	var promedio;
+	var tipoProducto;
+	var precio;
+	var cantidadUnidades;
+	var marca;
+	var fabricante;
+	var precioDeAlcoholMasBarato;
+	var contadorAlcohol;
+	var contadorBarbijos;
+	var contadorJabones;
+	var acumuladorAlcohol;
+	var acumuladorBarbijos;
+	var acumuladorJabones;
+	var unidadesAlcoholBarato;
+	var marcaAlcoholBarato;
+	var fabricanteAlcoholBarato;
+	var tipoProductoMasUnidades;
+
+
+
+	contador = 0;
+	contadorAlcohol = 0;
+	contadorBarbijos = 0;
+	contadorJabones = 0;
+	acumuladorJabones = 0;
+	acumuladorBarbijos = 0;
+	acumuladorAlcohol = 0;
+
+	while(contador < 5)
+	{
+		tipoProducto = prompt("Ingrese el tipo de producto");
+		while(tipoProducto != "barbijo" && tipoProducto != "jabón" && tipoProducto != "alcohol")
+		{
+			tipoProducto = prompt("Ingrese nuevamente otro tipo de producto");
+		}
+
+		contador++;
+
+		precio = prompt("Ingrese el precio del producto");
+		precio = parseFloat(precio);
+
+		while(precio < 100 || precio > 300)
+		{
+			precio = prompt("Ingrese nuevamente el precio del producto");
+			precio = parseFloat(precio);
+		}
+
+		cantidadUnidades = prompt("Ingrese la cantidad de unidades");
+		cantidadUnidades = parseInt(cantidadUnidades);
+
+		while(cantidadUnidades < 1 || cantidadUnidades > 1000)
+		{
+			cantidadUnidades = prompt("Ingrese nuevamente la cantidad de unidades");
+			cantidadUnidades = parseInt(cantidadUnidades);
+		}
+
+		marca = prompt("Ingrese la marca");
+
+		fabricante = prompt("Ingrese el fabricante");
+
+		switch(tipoProducto)
+		{
+			case "alcohol":
+				contadorAlcohol++;
+				acumuladorAlcohol = acumuladorAlcohol + cantidadUnidades;
+				if(precio < precioDeAlcoholMasBarato || contadorAlcohol == 1)
+				{
+					precioDeAlcoholMasBarato = precio;
+					unidadesAlcoholBarato = cantidadUnidades;
+					fabricanteAlcoholBarato = fabricante;
+					marcaAlcoholBarato = marca;
+				}
+			break;
+			
+
+			case "jabón":
+				contadorJabones++;
+				acumuladorJabones = acumuladorJabones + cantidadUnidades;
+			break;
+			case "barbijo":
+				contadorBarbijos++;
+				acumuladorBarbijos = acumuladorBarbijos + cantidadUnidades;
+			break;
+		}
+		//b) Del tipo con mas unidades, el promedio por compra
+		
+
+	}
+	if(acumuladorAlcohol>acumuladorJabones && acumuladorAlcohol > acumuladorBarbijos)
+		{
+			promedio = acumuladorAlcohol / contadorAlcohol;
+			tipoProductoMasUnidades = "alcohol";
+		}
+		else
+		{
+			if(acumuladorJabones>acumuladorBarbijos && acumuladorJabones>acumuladorAlcohol)
+			{
+				promedio = acumuladorJabones / contadorJabones;
+				tipoProductoMasUnidades= "jabón";
+			}
+			else
+			{
+				//barbijo
+				promedio = acumuladorBarbijos / contadorBarbijos;
+				tipoProductoMasUnidades = "barbijo";
+			}
+		}
+	if (contadorAlcohol==0)
+	{
+		document.write("A- No se ingresó ningun alcohol. <br>");
+	}
+	else
+	{
+		document.write("A- La cantidad de unidades del alcohol mas barato es: " + unidadesAlcoholBarato + " el fabricante es: " 
+		+ fabricanteAlcoholBarato + " de la marca: " + marcaAlcoholBarato + "<br>");
+	}
+	
+	document.write("B- El tipo con mas unidades es: " + tipoProductoMasUnidades + " con un promedio de: " + promedio + "<br>");
+	document.write("C- Hay " + acumuladorJabones + " jabones.");
 }
+
